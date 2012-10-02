5d6471f2056deb54ad86f0fa06473e324d64eb0c
spoljo <spoljo@diznilend.org>
2012-10-02 15:23:14 +0200

    Simpler way of dispatching messages
    Using dictionary code is clearer and easier
    to debug , cus if it fails you will see what segement fails
    from trackback.
    
    It works simple , it checkes if command is in dictionary ,
    and if it is , dictionary returns a function that is executed.

--- skypipe/satellite/server.py
+++ skypipe/satellite/server.py
@@ -1,11 +1,6 @@
-"""Skypipe server
-
-This implements the server side of skypipe. Thanks to
-ZeroMQ it's even simpler than the client. So just read it!
-"""
 import collections
-import os
 import sys
+import os
 import zmq
 
 SP_HEADER = "SKYPIPE/0.1"
@@ -25,6 +20,41 @@ pipe_clients = collections.defaultdict(list) # connected skypipe clients
 pipe_buffers = collections.defaultdict(list) # any buffered data for pipes
 
 print "Serving on {}...".format(port)
+
+checker = {
+        SP_CMD_LISTEN :cmd_lis_func,
+        SP_CMD_UNLISTEN: cmd_unlis_func,
+        SP_CMD_DATA: cmd_data_func,
+        }
+
+def cmd_lis_func():
+    pipe_clients[pipe_name].append(client)
+    if len(pipe_clients[pipe_name]) == 1:
+        # if only client after adding, then previously there were
+        # no clients and it was buffering, so spit out buffered data
+        while len(pipe_buffers[pipe_name]) > 0:
+            data = pipe_buffers[pipe_name].pop(0)
+            router.send_multipart([client,
+                SP_HEADER, SP_CMD_DATA, pipe_name, data])
+            if data == SP_DATA_EOF:
+                # remember this kicks the client, so stop
+                # sending data until the next one listens
+                break
+
+def cmd_unlis_func():
+    if client in pipe_clients[pipe_name]:
+        pipe_clients[pipe_name].remove(client)
+ 
+def cmd_data_func():
+    data = msg.pop(0)
+    if not pipe_clients[pipe_name]:
+        pipe_buffers[pipe_name].append(data)
+    else:
+        for listener in pipe_clients[pipe_name]:
+            router.send_multipart([listener,
+                SP_HEADER, SP_CMD_DATA, pipe_name, data])
+ 
+
 while True:
     sys.stdout.flush()
     
@@ -41,37 +71,10 @@ while True:
     
     else:
         pipe_name = msg.pop(0)
-    
-        if SP_CMD_LISTEN == command:
-            pipe_clients[pipe_name].append(client)
-            if len(pipe_clients[pipe_name]) == 1:
-                # if only client after adding, then previously there were
-                # no clients and it was buffering, so spit out buffered data
-                while len(pipe_buffers[pipe_name]) > 0:
-                    data = pipe_buffers[pipe_name].pop(0)
-                    router.send_multipart([client,
-                        SP_HEADER, SP_CMD_DATA, pipe_name, data])
-                    if data == SP_DATA_EOF:
-                        # remember this kicks the client, so stop
-                        # sending data until the next one listens
-                        break
-        
-        elif SP_CMD_UNLISTEN == command:
-            if client in pipe_clients[pipe_name]:
-                pipe_clients[pipe_name].remove(client)
-        
-        elif SP_CMD_DATA == command:
-            data = msg.pop(0)
-            
-            if not pipe_clients[pipe_name]:
-                pipe_buffers[pipe_name].append(data)
-            else:
-                for listener in pipe_clients[pipe_name]:
-                    router.send_multipart([listener,
-                        SP_HEADER, SP_CMD_DATA, pipe_name, data])
-        
-        else:
+        try:
+            checker[command]()
+        except KeyError:
             print client_display, "Unknown command:", command
-            continue
+
 
     print client_display, command
